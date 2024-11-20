3e480136a62b55cfd9351bf29e5144538b85bfa3
costarelli marco <costarelli.marco@iisgalvanimi.edu.it>
2024-11-20 09:33:13 +0000

    aggiunto metodoto per visualizzare lista

--- listaSpesa.py
+++ listaSpesa.py
@@ -2,4 +2,6 @@ lista=[]
 def agg_elemento(x, lista):
     lista.append(x) #l'input lo facciamo nel main
 
-
+def visua_elemento(lista):
+    for i in range(len(lista)):
+    print(f"{i + 1}. {lista[i]}")
